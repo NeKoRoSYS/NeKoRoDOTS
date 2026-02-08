8a8ddbe102643455279048006944263e84099971
NeKoRoSYS <nekorosys@gmail.com>
2026-02-09 07:01:09 +0800

    Add files via upload

--- .config/hypr/scripts/change-lockscreen.sh
+++ .config/hypr/scripts/change-lockscreen.sh
@@ -7,18 +7,14 @@ pkill -x wofi
 SKIN_DIR="$HOME/.config/hypr/hyprlock/skins"
 MAIN_HYPRLOCK="$HOME/.config/hypr/hyprlock.conf"
 
-notify-send -a "Hyprlock" -h string:x-canonical-private-synchronous:hyprlock "Hyprlock" "Select a lockscreen skin."
+notify-send -a "Hyprlock" -h string:x-canonical-private-synchronous:hyprlock "Hyprlock" "Select a lockscreen layout."
 
 # Get list of skins (files ending in .conf)
-CHOICE=$(ls "$SKIN_DIR" | grep '\.conf$' | wofi --dmenu --prompt "Select Hyprlock Skin")
+CHOICE=$(ls "$SKIN_DIR" | grep '\.conf$' | wofi --dmenu --prompt "Select Hyprlock Layout")
 
 if [ -n "$CHOICE" ]; then
-    # Define the full path for the selected skin
     SELECTED_SKIN_PATH="$SKIN_DIR/$CHOICE"
-
-    # Update hyprlock.conf to source the new skin
-    # This overwrites the file with a single source line pointing to the selection
     echo "source = $SELECTED_SKIN_PATH" > "$MAIN_HYPRLOCK"
 
-    notify-send -a "Hyprlock" -h string:x-canonical-private-synchronous:hyprlock -i preferences-desktop-theme "Hyprlock" "Skin changed to: ${CHOICE%.conf}"
+    notify-send -a "Hyprlock" -h string:x-canonical-private-synchronous:hyprlock -i preferences-desktop-theme "Hyprlock" "Layout changed to: ${CHOICE%.conf}"
 fi
